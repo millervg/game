bea566f86a070268f43a48499063a6f28b4ca070
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-01 11:03:24 -0600

    added die.h

---
+++ content/events/die.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "event.h"
+
+// forward declaration
+class Actor;
+
+class Die : public Event {
+public:
+    Die(Actor& actor);
+    void execute(Engine& engine) override;
+private:
+    Actor& actor;
+};
