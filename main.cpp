c4ec1e0beff261b97dcbe24b3a85a3a7e924b3c4
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-01 21:08:45 -0600

    added two new monsters to main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "engine.h"
 #include "heros.h"
+#include "monsters.h"
 
 int main() {
     std::string filename{"settings.txt"};
@@ -7,5 +8,11 @@ int main() {
     Engine engine{settings};
 
     engine.create_hero(Heros::knight);
+    MonsterType new_monster_type = Monsters::goblin();
+    engine.create_monster(new_monster_type);
+    MonsterType new_monster_type2 = Monsters::skeleton();
+    engine.create_monster(new_monster_type2);
+    MonsterType new_monster_type3 = Monsters::zombie();
+    engine.create_monster(new_monster_type3);
     engine.run();
 }
