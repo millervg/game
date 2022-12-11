fe56d9f5d6d27c8f774770d26d67b05f0094c249
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-11 17:40:16 -0600

    created hammer.cpp

---
+++ content/weapons/hammer.cpp
@@ -0,0 +1,12 @@
+#include "hammer.h"
+#include "hit.h"
+#include "engine.h"
+#include "swing.h"
+
+Hammer::Hammer(int damage)
+    :Weapon{"hammer", damage} {}
+
+void Hammer::use(Engine& engine, Actor& attacker, Actor& defender) {
+    Vec direction = defender.get_position() - attacker.get_position();
+    engine.events.add(Swing{sprite, direction, defender, damage});
+}
