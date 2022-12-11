12ebc7eee2ebdf70261b36699e690ecbd9f42f86
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-11 16:29:40 -0600

    updated

--- content/actions/wander.cpp
+++ content/actions/wander.cpp
@@ -5,14 +5,14 @@
 #include "randomness.h"
 
 Result Wander::perform(Engine& engine) { 
-    Vec position = actor->get_position(); 
-    std::vector<Vec> neighbors = engine.dungeon.neighbors(position); 
+    Vec pos = actor->get_position(); 
+    std::vector<Vec> neighbors = engine.dungeon.neighbors(pos); 
     // randomize directions 
     shuffle(std::begin(neighbors), std::end(neighbors));  
     for (const Vec& neighbor : neighbors) { 
         Tile& tile = engine.dungeon.tiles(neighbor); 
         if (!tile.is_wall() && !tile.actor) { 
-            Vec direction = neighbor - position; 
+            Vec direction = neighbor - pos; 
             return alternative(Move{direction}); 
         } 
     }
