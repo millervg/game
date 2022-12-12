b1d2850db863fe66c66d8ed5ec9dbfee14c37e7d
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-12 01:51:10 -0600

    added attack

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -6,6 +6,9 @@
 #include "engine.h"
 #include "opendoor.h"
 #include "closedoor.h"
+#include "attack.h"
+#include "dungeon.h"
+#include "door.h"
 
 Move::Move(Vec direction)
     :direction(direction) {}
@@ -16,19 +19,22 @@ Result Move::perform(Engine& engine) {
     actor->change_direction(direction);
     
     Tile& tile = engine.dungeon.tiles(new_position);
-    if (tile.is_wall() || tile.actor) {
+    if (tile.is_wall()) {
         return failure();
     }
 
-      if (tile.is_door()) {
+    if (tile.is_door()) {
         Door& door = engine.dungeon.doors.at(new_position);
         if (!door.is_open()) {
             return alternative(OpenDoor{new_position});
         }
     }
 
-        actor->move_to(new_position);
-        return success();
+    if(tile.actor) {
+        return alternative(Attack{*tile.actor});
+    }
 
+    actor->move_to(new_position);
+    return success();
 }   
 
