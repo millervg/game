9a5925e6e619c2f72fcf195c5e16c8bfc6837346
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-12 13:05:26 -0600

    added spin effect

--- content/weapons/hammer.cpp
+++ content/weapons/hammer.cpp
@@ -1,12 +1,11 @@
 #include "hammer.h"
 #include "hit.h"
 #include "engine.h"
-#include "swing.h"
-
+#include "spin.h"
 Hammer::Hammer(int damage)
     :Weapon{"hammer", damage} {}
 
 void Hammer::use(Engine& engine, Actor& attacker, Actor& defender) {
     Vec direction = defender.get_position() - attacker.get_position();
-    engine.events.add(Swing{sprite, direction, defender, damage});
+    engine.events.add(Spin{sprite, direction, defender, damage});
 }
