7eea9debfb97a4ee42e0f65e76c9d741291d5606
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-12 13:05:41 -0600

    created spin.cpp

---
+++ content/events/spin.cpp
@@ -0,0 +1,44 @@
+#include "spin.h"
+#include "event.h"
+#include "engine.h"
+#include "hit.h"
+#include <cmath>
+
+constexpr int duration = 20;
+
+Spin::Spin(Sprite& sprite, Vec direction, Actor& defender, int damage)
+    :Event{duration}, sprite{sprite}, copy{sprite},
+    defender{defender}, damage{damage} {
+
+    // set starting_angle
+    // set delta (which rotation direction)
+    if (direction == Vec{1, 0}) {
+        starting_angle = 0;
+        delta = 360.0 / (duration - 1);
+    }
+    else if (direction == Vec{-1, 0}) {
+        starting_angle = 0;
+        delta = 360.0 / (duration - 1);
+    }
+    else if (direction == Vec{0, 1}) {
+        sprite.shift.y -= 12;
+        double sign = std::copysign(1.0, starting_angle);
+        starting_angle = -75 * sign;
+        delta = 360.0 / (duration - 1) * sign;
+    }
+    else { // {0, -1}
+        sprite.shift.x = 0;
+        double sign = std::copysign(1.0, starting_angle);
+        starting_angle = 135 * sign;
+        delta = 360.0 / (duration - 1) * sign;
+    }
+}
+
+void Spin::execute(Engine&) {
+    sprite.angle = starting_angle + delta * frame_count;
+}
+
+void Spin::when_done(Engine& engine) {
+    sprite = copy;
+    engine.events.add(Hit{defender, damage});
+}
