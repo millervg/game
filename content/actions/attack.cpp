0f9918e9eacd4f0bf887a3301b6e0f5271b7f2e1
Victoria G. Miller <vickigraceem@gmail.com>
2022-12-12 01:50:46 -0600

    updated attack.cpp

--- content/actions/attack.cpp
+++ content/actions/attack.cpp
@@ -1,1 +1,10 @@
 #include "attack.h"
+#include "engine.h"
+
+Attack::Attack(Actor& defender) 
+    : defender{defender} {}
+
+Result Attack::perform(Engine&) {
+    actor->attack(defender);
+    return success();
+}
